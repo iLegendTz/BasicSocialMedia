cccf726e9b7539d1b71508d8ef6c177d53e687d8
Isaac <miguel.isaac@live.com>
2019-05-24 13:36:40 -0500

    Se crea js/main.js, se crea includes/layouts/header.php el cual implementa todas las 'Hojas de Estilo', implementa 'reCaptcha' y configura la zona horaria del servidor PHP a 'America/Mexico_City', se crea includes/layouts/footer.php el cual implementa todos los archivos JS

---
+++ includes/layouts/footer.php
@@ -0,0 +1,3 @@
+<script src="js/jquery.js"></script>
+<script src="includes/bootstrap4/js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
---
+++ includes/layouts/header.php
@@ -0,0 +1,11 @@
+<?php date_default_timezone_set('America/Mexico_City'); ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="includes/bootstrap4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
